12d9a445d1283c9e117e1771406f20619ed59eed
pikka1999 <h_tanaka@toridori.co.jp>
2024-09-20 17:54:34 +0900

    testエンドポイント作成

---
+++ src/test/test.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller("test")
+export class TestController {
+    constructor() {}
+
+    @Get()
+    async test() {
+        return 'test';
+    }
+}
