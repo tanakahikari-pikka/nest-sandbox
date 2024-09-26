70b82aedeeae27d656a3822ff4856d42733e394f
pikka1999 <h_tanaka@toridori.co.jp>
2024-09-26 11:04:51 +0900

    許可するcorsポリシーの追加

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // 許可するHTTPメソッド
+    allowedHeaders: 'Content-Type, Authorization',  // 許可するヘッダー
+  });
   await app.listen(80);
 }
 bootstrap();
